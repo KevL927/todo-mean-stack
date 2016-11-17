f38698491e12cb200f14983d5511223adceb1d19
KL012 <kevl927@gmail.com>
2016-11-17 05:00:47 +0000

    setup express and get method route

---
+++ src/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use('/', express.static('public'));
+
+var router = express.Router();
+
+router.get('/todos', function(req, res) {
+    res.json({todo: []});
+});
+
+app.use('/api', router);
+
+app.listen(8080, function () {
+    console.log('The server is running on port 8080.');
+});
